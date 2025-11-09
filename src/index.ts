7adc0fd744a9e10210413611b78f32ba34e4818d
coding-zero-I <yashsangwan4700@gmail.com>
2025-11-09 20:39:05 +0530

    added user router

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import {config} from 'dotenv';
+import cors from 'cors';
+import userRouter from './routes/userRoutes.js';
+import connectDB from './config/db.js';
 
 config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors())
 
-app.listen(PORT, () => {
+app.use('/api/user',userRouter);
+app.listen(PORT, async() => {
+  await connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
