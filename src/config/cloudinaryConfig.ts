68c31cee607412ff883a10a47fbfcdd1bc930d97
coding-zero-I <yashsangwan4700@gmail.com>
2025-11-11 10:06:49 +0530

    added cloudinary and multer config

---
+++ src/config/cloudinaryConfig.ts
@@ -0,0 +1,18 @@
+import { v2 } from "cloudinary";
+
+const configureCloudinary = () => {
+  if (
+    !process.env.CLOUDINARY_CLOUD_NAME ||
+    !process.env.CLOUDINARY_API_KEY ||
+    !process.env.CLOUDINARY_API_SECRET
+  ) {
+    throw new Error("Cloudinary configuration variables are missing");
+  }
+
+  v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
+    api_key: process.env.CLOUDINARY_API_KEY as string,
+    api_secret: process.env.CLOUDINARY_API_SECRET as string,
+  });
+};
+export default configureCloudinary;
